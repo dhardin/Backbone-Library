13bc9ecad8b0f0a817b4fdd8de751ff5587b52ff
dhardin <dhardin@ltu.edu>
2014-08-03 12:46:42 -0400

    added search functionality for library (aka, regex search)

--- public/js/models/library.js
+++ public/js/models/library.js
@@ -1,5 +1,25 @@
 var app = app || {};
 
 app.Library = Backbone.Collection.extend({
-	model: app.Book
+	model: app.Book,
+
+	search: function (text) {
+		var regex, key;
+
+		if (text.length == 0) {
+			return this;
+		}
+
+		regex = new RegExp(text, "gi");
+
+		return _(this.filter(function(data) {
+			for (key in data.attributes){
+				if (regex.test(data.attributes[key])){
+					return true;
+				}
+			}
+			return false;
+		}));
+
+	}
 });
--- public/js/views/library.js
+++ public/js/views/library.js
@@ -4,22 +4,31 @@ app.LibraryView = Backbone.View.extend({
 	template: _.template($('#libraryTemplate').html()),
 	id: '#libraryContent',
 
-
-
+	events: {
+		'keyup #search' : 'search',
+	},
 
 	initialize: function (initialBooks){
 		this.collection = app.LibraryCollection;
 		this.render();
+		
 
 		this.listenTo(this.collection, 'add', function(){app_router.navigate('//library', { trigger: true });});
 		this.listenTo(this.collection, 'reset', this.render)
 	},
 
-	
-	render: function () {
+	render: function (collection, filterText) {
 		this.$el.html(this.template());
 		this.$books = this.$el.find('#books'); 	
+		this.$filter = this.$el.find('#search');
 		this.collection.each(function(item){
+			this.renderBook(item);
+		}, this);
+	},
+
+	renderList: function(collection){
+		this.$books.html('');
+		collection.each(function(item){
 			this.renderBook(item);
 		}, this);
 	},
@@ -29,5 +38,10 @@ app.LibraryView = Backbone.View.extend({
 			model: item
 		});
 		this.$books.append(bookView.render().el);
+	},
+
+	search: function(e){
+		var text = this.$filter.val();
+		this.renderList(this.collection.search(text));
 	}
 });
